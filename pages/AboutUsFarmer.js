600ccc739a85c5ee1b036a27fbabb706c5cd24e9
Awais Khan <ssc.awaiskhan.2409@gmail.com>
2024-11-25 11:43:29 +0500

    ContactUs and AboutUS Farmer Specific

---
+++ pages/AboutUsFarmer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Hero from "../components/FarmerHero";
+import AboutUsComponent from "../components/AboutUsComponent";
+import Footer from "@/components/Footer";
+
+const ContactUs = () => {
+  return (
+    <>
+      <Hero/>
+      <AboutUsComponent />
+      <Footer />
+    </>
+  );
+};
+
+export default ContactUs;
